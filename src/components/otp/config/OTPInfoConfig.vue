4cc0d95125b32c27434d5d4e82481df7e696bca0
Seedgou <i@zczc.cz>
2023-01-29 17:10:20 +0000

    add collapse for config

--- src/components/otp/config/OTPInfoConfig.vue
+++ src/components/otp/config/OTPInfoConfig.vue
@@ -1,45 +1,60 @@
 <template>
-  <span>
-    <n-form>
-      <OTPURIInput v-model:options="otp.options" />
-
-      <n-form-item label="Type">
-        <n-switch v-model:value="typeComputed">
-          <template #checked>TOTP</template>
-          <template #unchecked>HOTP</template>
-        </n-switch>
-      </n-form-item>
-      <n-form-item label="Label">
-        <n-input v-model:value="otp.options.label" placeholder="Label" />
-      </n-form-item>
-      <n-form-item label="Issuer">
-        <n-input v-model:value="otp.options.issuer" placeholder="Issuer" />
-      </n-form-item>
-      <n-form-item label="Secret">
-        <n-input
-          v-model:value="otp.options.secret"
-          placeholder="Secret"
-          type="password"
-          show-password-on="click"
-        />
-      </n-form-item>
-      <n-form-item label="Algorithm">
-        <n-select
-          v-model:value="otp.options.algorithm"
-          :options="algorithmOptions"
-        />
-      </n-form-item>
-      <n-form-item label="Period" v-if="otp.options.type === 'totp'">
-        <n-input-number v-model:value="otp.options.period" :min="0" />
-      </n-form-item>
-      <n-form-item label="Counter" v-if="otp.options.type === 'hotp'">
-        <n-input-number v-model:value="otp.options.counter" :min="0" />
-      </n-form-item>
-      <n-form-item label="Digits">
-        <n-input-number v-model:value="otp.options.digits" :min="0" />
-      </n-form-item>
-    </n-form>
-  </span>
+  <n-collapse>
+    <n-collapse-item title="Key URI" name="key uri">
+      <div><OTPURIInput v-model:options="otp.options" /></div>
+    </n-collapse-item>
+    <n-collapse-item title="Parameters" name="parameters">
+      <div>
+        <n-form-item label="Type">
+          <n-switch v-model:value="typeComputed">
+            <template #checked>TOTP</template>
+            <template #unchecked>HOTP</template>
+          </n-switch>
+        </n-form-item>
+        <n-form-item label="Label">
+          <n-input v-model:value="otp.options.label" placeholder="Label" />
+        </n-form-item>
+        <n-form-item label="Issuer">
+          <n-input v-model:value="otp.options.issuer" placeholder="Issuer" />
+        </n-form-item>
+        <n-form-item label="Secret">
+          <n-input
+            v-model:value="otp.options.secret"
+            placeholder="Secret"
+            type="password"
+            show-password-on="click"
+          />
+        </n-form-item>
+        <n-form-item label="Algorithm">
+          <n-select
+            v-model:value="otp.options.algorithm"
+            :options="algorithmOptions"
+          />
+        </n-form-item>
+        <n-form-item label="Period" v-if="otp.options.type === 'totp'">
+          <n-input-number
+            v-model:value="otp.options.period"
+            :min="0"
+            placeholder="30"
+          />
+        </n-form-item>
+        <n-form-item label="Counter" v-if="otp.options.type === 'hotp'">
+          <n-input-number
+            v-model:value="otp.options.counter"
+            :min="0"
+            placeholder="0"
+          />
+        </n-form-item>
+        <n-form-item label="Digits">
+          <n-input-number
+            v-model:value="otp.options.digits"
+            :min="0"
+            placeholder="6"
+          />
+        </n-form-item>
+      </div>
+    </n-collapse-item>
+  </n-collapse>
 </template>
 
 <script setup lang="ts">
@@ -48,12 +63,13 @@ import type { OTPInfo } from "@/data/otp";
 import { useVModel } from "@vueuse/core";
 import OTPURIInput from "./OTPURIInput.vue";
 import {
-  NForm,
   NFormItem,
   NInput,
   NSelect,
   NInputNumber,
   NSwitch,
+  NCollapse,
+  NCollapseItem,
 } from "naive-ui";
 
 const props = defineProps<{
