ab88f9ca094cdf60b1a52e0eab3dc734990f1e67
Seedgou <i@zczc.cz>
2023-01-30 12:55:47 +0000

    add screen capture import

--- src/components/otp/import/ImportButton.vue
+++ src/components/otp/import/ImportButton.vue
@@ -12,18 +12,21 @@
     </n-dropdown>
 
     <URIImport v-model:show="shows.uri" />
+    <ScreenCaptureImport v-model:show="shows.screen" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, h, type Component } from "vue";
 import URIImport from "./uri/URIImport.vue";
+import ScreenCaptureImport from "./screen/ScreenCaptureImport.vue";
 import { NButton, NIcon, NDropdown } from "naive-ui";
 import AddSquareMultiple16Regular from "@vicons/fluent/AddSquareMultiple16Regular";
 import Link16Regular from "@vicons/fluent/Link16Regular";
 
 const shows = ref<Record<string, boolean>>({
   uri: false,
+  screen: false,
 });
 
 const renderIcon = (icon: Component) => {
@@ -38,6 +41,11 @@ const options = [
   {
     label: "URI",
     key: "uri",
+    icon: renderIcon(Link16Regular),
+  },
+  {
+    label: "Screen Capture",
+    key: "screen",
     icon: renderIcon(Link16Regular),
   },
 ];
