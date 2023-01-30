1314036c0ca7bd1711337872a4abadb05832e915
Seedgou <i@zczc.cz>
2023-01-30 16:05:46 +0000

    format token to two parts

--- src/components/otp/display/otp/OTPToken.vue
+++ src/components/otp/display/otp/OTPToken.vue
@@ -1,17 +1,22 @@
 <template>
   <span>
-    <!-- {{ token }} -->
-    {{ tokenAnimated }}
+    <span style="margin-right: 0.4em">{{ firstPart }}</span
+    ><span>{{ secondPart }}</span>
   </span>
 </template>
 
 <script setup lang="ts">
-import { toRef } from "vue";
-import { useTokenAnimated } from "@/composables/otp/useTokenAnimated";
+import { computed } from "vue";
+// import { useTokenAnimated } from "@/composables/otp/useTokenAnimated";
 
 const props = defineProps<{
   token: string;
 }>();
 
-const tokenAnimated = useTokenAnimated(toRef(props, "token"));
+// divide token to two part
+const divideIndex = computed(() => Math.round(props.token.length / 2));
+const firstPart = computed(() => props.token.slice(0, divideIndex.value));
+const secondPart = computed(() => props.token.slice(divideIndex.value));
+
+// const tokenAnimated = useTokenAnimated(toRef(props, "token"));
 </script>
