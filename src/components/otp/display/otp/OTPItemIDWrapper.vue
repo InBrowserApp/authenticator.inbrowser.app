0505945ee072b9df27abbd6a84d01eb0c2d5512a
Seedgou <i@zczc.cz>
2023-01-30 10:25:29 +0000

    useOTPInfo

--- src/components/otp/display/otp/OTPItemIDWrapper.vue
+++ src/components/otp/display/otp/OTPItemIDWrapper.vue
@@ -6,23 +6,11 @@
 
 <script setup lang="ts">
 import OTPItem from "@/components/otp/display/otp/OTPItem.vue";
-import type { OTPInfo } from "@/data/otp";
-import { useStorage } from "@vueuse/core";
+import { useOTPInfo } from "@/data/otp/composables/useOTPInfo";
 
 const props = defineProps<{
   id: string;
 }>();
 
-const info = useStorage<OTPInfo>(`authenticator:otp:${props.id}`, {
-  id: props.id,
-  options: {
-    type: "totp" as const,
-    secret: "KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD",
-    issuer: "Test",
-    label: "Test",
-    algorithm: "SHA1" as const,
-    digits: 6 as const,
-    period: 30 as const,
-  },
-});
+const info = useOTPInfo(props.id);
 </script>
---
+++ src/data/otp/composables/useOTPInfo.ts
@@ -0,0 +1,19 @@
+import type { OTPInfo } from "@/data/otp";
+import { useStorage } from "@vueuse/core";
+
+export function useOTPInfo(id: string) {
+  const info = useStorage<OTPInfo>(`authenticator:otp:${id}`, {
+    id: id,
+    options: {
+      type: "totp" as const,
+      secret: "KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD",
+      issuer: "Test Issuer",
+      label: "Test Label",
+      algorithm: "SHA1" as const,
+      digits: 6 as const,
+      period: 30 as const,
+    },
+  });
+
+  return info;
+}
