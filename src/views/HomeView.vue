2dd68cab058ba50bbf765d987d0956f1db195bce
Seedgou <i@zczc.cz>
2023-02-04 13:28:39 +0000

    add @vueuse/head

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,24 @@
 <script setup lang="ts">
 import OTPItemIDWrapper from "@/components/otp/display/otp/OTPItemIDWrapper.vue";
 import { useOrderStore } from "@/stores/order";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "Authenticator InBrowser.App",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Authenticator InBrowser.App is an offline-capable TOTP & HOTP Authenticator. All data stay securely in your browser.",
+    },
+  ],
+  link: [
+    {
+      rel: "canonical",
+      href: `https://authenticator.inbrowser.app/`,
+    },
+  ],
+});
 
 const orderStore = useOrderStore();
 </script>
